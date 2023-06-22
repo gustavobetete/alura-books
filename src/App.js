cd40f9ea798a7f790797e275e952589988148716
gustavobetete <gustavobetete@hotmail.com>
2023-06-22 16:42:15 -0300

    feat: usando loops

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
 import Logo from './components/logo/index.js';
+import perfil from './assets/perfil.svg'
+import sacola from './assets/sacola.svg'
+
+const textoOpcoes = ['CATEGORIAS', 'MINHA ESTANTE', 'FAVORITOS']
+const icones = [perfil, sacola]
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <Logo />
+        <ul className="opcoes">
+          { textoOpcoes.map((texto) => 
+            <li className='opcao'><p>{texto}</p></li>)}
+        </ul>
+
+        <ul className="icones">
+          { icones.map((icone) => (
+            <li className='icone'><img src={icone} alt='icone' /></li>
+          ))}
+        </ul>
       </header>
     </div>
   );
