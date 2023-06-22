55f7b016f97e2faa767404da1a63908eaa3d5821
gustavobetete <gustavobetete@hotmail.com>
2023-06-22 15:01:45 -0300

    feat: Estilizando

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 import Logo from './components/logo/index.js';
 
 function App() {
--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -1,9 +1,14 @@
+import './style.css'
 import logoAlura from '../../assets/logo.svg'
 
 function Logo() {
     return (
         <div className='logo'>
-          <img src={logoAlura} alt='Logo' />
+          <img 
+            src={logoAlura} 
+            alt='Logo' 
+            className='logo-img'
+          />
           <p><strong>Alura</strong>Books</p>
         </div>
     );
